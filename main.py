89ac413749e3fdca7dca32a23873c787a31c6022
nosdyE <114820751+eydson@users.noreply.github.com>
2025-05-27 16:29:45 -0300

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from discord.ext import commands
 import re
 from collections import defaultdict
 
+# TOKEN do Bot
+TOKEN = os.getenv('TOKEN')
+
 # Configure intents
 intents = discord.Intents.default()
 intents.message_content = True
